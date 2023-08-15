3e4988e3207db1aa29fbca3197a64806b9cefe76
Charly <beaussart@protonmail.com>
2023-08-15 22:13:01 +0200

    v2

--- home/urls.py
+++ home/urls.py
@@ -6,7 +6,7 @@ from .views import tables, prediction, update_data, get_products_by_label
 
 urlpatterns = [
   path('' , views.index, name='index'),
-  # path('tables/', tables     , name='tables'),
+  path('tables/', tables     , name='tables'),
   path('update_data/', update_data, name='update_data'),
   path('get_products_by_label/', get_products_by_label, name='get_products_by_label'),
   # Components
